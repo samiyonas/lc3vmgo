6bf2fee604624f6ff2f35d5aae642a4e4de98104
samiyonas <samuel.x.yonas@gmail.com>
2025-03-25 01:14:31 +0300

    registers created

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
-var MEMORY_MAX = 1 << 16;
+const MEMORY_MAX = 1 << 16;
 var memory[MEMORY_MAX]uint16;
+
+const (
+    R_R0 = iota,
+    R_R1
+    R_R2
+    R_R3
+    R_R4
+    R_R5
+    R_R6
+    R_R7
+    R_PC /* program counter */
+    R_COND /* condition flag */
+    R_COUNT
+)
+
+var reg[R_COUNT]uint16;
