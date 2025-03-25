a98d755fb2a4d63b7975a58c9bf4ea1fddbbcf6e
samiyonas <samuel.x.yonas@gmail.com>
2025-03-25 13:23:21 +0300

    added opcodes and condition flags

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ const MEMORY_MAX = 1 << 16;
 var memory[MEMORY_MAX]uint16;
 
 const (
-    R_R0 = iota,
+    R_R0 = iota
     R_R1
     R_R2
     R_R3
@@ -18,3 +18,28 @@ const (
 )
 
 var reg[R_COUNT]uint16;
+
+const (
+    OP_BR = iota /* branch */
+    OP_ADD /* add */
+    OP_LD /* load */
+    OP_ST /* store */
+    OP_JSR /* jump register */
+    OP_AND /* bitwise and */
+    OP_LDR /* load register */
+    OP_STR /* store register */
+    OP_RTI /* unused */
+    OP_NOT /* bitwise not */
+    OP_LDI /* load indirect */
+    OP_STI /* store indirect */
+    OP_JMP /* jump */
+    OP_RES /* reserved (unused) */
+    OP_LEA /* load effective address */
+    OP_TRAP /* execute trap */
+)
+
+const (
+    FL_POS = 1 << 0
+    FL_ZRO = 1 << 1
+    FL_NEG = 1 << 2
+)
